59ffe7c98fdf31122efec0170011b73ab3801cb1
Vermouth-M <decadedxtsu@gmail.com>
2025-11-29 01:49:00 +0700

    tambahan sedikit buat fungsi draw

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@
 class Partikel {
 public:
     sf::Vector2f position;
-    sf::Vector2f velocity;
+    float speed;
     float radius;
     sf::Color color;
     float mass;
 
-    Partikel(float x, float y, float r, sf::Color c) 
-        : position(x, y), radius(r), color(c) {
+    Partikel(float x, float y, float r, sf::Color c, float vel) 
+        : position(x, y), radius(r), color(c), speed(vel) {
         mass = r * r; 
     }
+    void draw(sf::RenderWindow& window){
+        sf::CircleShape lingkaran({radius});
+        lingkaran.setPosition({position.x-radius, position.y-radius});
+
+    }
+
 };
 
 int main() {
