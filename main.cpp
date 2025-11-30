3a16d39392d4b265464032ff018cf3789fa1333c
Vermouth-M <decadedxtsu@gmail.com>
2025-11-30 14:35:40 +0700

    fitur - sudah ada physicsnya karena sudah bukan transfer velocity namun juga sudah ada rumusnya(rumus momentum) bug - kalau berumbuk di ujung border bisa menghilang bolanya

--- main.cpp
+++ main.cpp
@@ -26,11 +26,17 @@ public:
         window.draw(lingkaran);
     }
 
-    void updatekondisi(float lebar, float tinggi) {
+    void updatekondisi(float lebar, float tinggi, float dt) {
         // Update posisi
-        position += velocity * 0.016f;
+        position += velocity * dt;
+        float kecepatanMaks = 500.0f;
+        float kecepatan = sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
 
-        // Pantul
+        if (kecepatan > kecepatanMaks){
+            velocity = velocity * (kecepatanMaks / kecepatan);
+        }
+
+        // Pantul border
         if (position.x < radius || position.x > lebar - radius) {
             velocity.x = -velocity.x;
         }
@@ -45,12 +51,27 @@ public:
         return jarak < (radius + lain.radius);
     }
     void colission(Partikel& lain){
-        sf::Vector2f temp = velocity;
-        velocity = lain.velocity;
-        lain.velocity = temp;
-
         sf::Vector2f delta = position - lain.position;
         float jarak = sqrt(delta.x * delta.x + delta.y * delta.y);
+        if (jarak == 0){
+            return;
+        }
+        // mantul harus sesuai dgn rumus tumbukan
+        // rumus rumbukan = v' = (Mass*velocity+Mass.lain*velocity.lain)/mass+mass.lain
+        sf::Vector2f arah = delta / jarak;
+        //percepatan awal
+        float arahv1 = velocity.x * arah.x + velocity.y * arah.y;
+        float arahv2 = lain.velocity.x * arah.x + lain.velocity.y * arah.y;
+        //percepatan after tumbukan
+        float v1_1 = ((mass - lain.mass) * arahv1 + lain.mass * arahv2) / (mass + lain.mass);
+        float v2_1 = ((lain.mass - mass) * arahv2 + lain.mass * arahv1) / (mass + lain.mass);
+        // perhitungan total (awal-setelah(agar tdk minus))
+        float totalperhitungan_1 = v1_1 - arahv1;
+        float totalperhitungan_2 = v2_1 - arahv2;
+
+        velocity += arah * totalperhitungan_1;
+        lain.velocity += arah * totalperhitungan_2;
+
         if (jarak > 0) {
             float overlap = (radius + lain.radius - jarak) / 2.0f;
             sf::Vector2f arah = delta / jarak;
@@ -63,12 +84,13 @@ public:
 //class untuk quadtree
 class Quadtree {
 
-}
+};
 
 int main() {
     srand(time(0));
     
     vector<Partikel> listpartikel;
+    sf::Clock clock;
     sf::Color color[]{
         sf::Color::Red, sf::Color::Green, sf::Color::Blue,
         sf::Color::Yellow, sf::Color::Magenta, sf::Color::Cyan
@@ -87,7 +109,7 @@ int main() {
             float x = 50 + rand() % (LEBAR - 100);
             float y = 50 + rand() % (TINGGI - 100);
             ceklokasi = true;
-            for (int j = 0;listpartikel.size()<j;j++){
+            for (int j = 0;j < listpartikel.size()<j;j++){
                 float tempx = x-listpartikel[j].position.x;
                 float tempy = y-listpartikel[j].position.y;
                 float jarak = sqrt(tempx*tempx+tempy*tempy);
@@ -110,6 +132,7 @@ int main() {
     }
 
     while(window.isOpen()) {
+        float waktu = clock.restart().asSeconds();
         while(const auto event = window.pollEvent()) {
             if(event->is<sf::Event::Closed>()) {
                 window.close();
@@ -118,7 +141,7 @@ int main() {
 
         // Update semua bola
         for(int i = 0; i < listpartikel.size(); i++) {
-            listpartikel[i].updatekondisi(LEBAR, TINGGI);
+            listpartikel[i].updatekondisi(LEBAR, TINGGI, waktu);
         }
         //Cek tumbukan(brute force utk saat ini)
         for(int i = 0; i < listpartikel.size(); i++) {
